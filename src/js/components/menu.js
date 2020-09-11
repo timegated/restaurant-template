0b578344b4fd5e691cd48c55458889a8aaf27d5c
dbeccaria826 <beccdaniel@gmail.com>
2020-09-10 21:36:47 -0400

    menu: working on creating a menu feature

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,14 +1,28 @@
+import { menuItems } from './menuitems/menuitems';
+
 export const menu = () => {
   const contentArea = document.querySelector('#content-area');
   const contentWrapper = document.createElement('div');
+  const title = document.createElement('h1');
+  const menuItemList = document.createElement('div');
+  menuItemList.classList.add('menu-items');
+  title.textContent = 'Menu';
   contentWrapper.id = 'content-area__wrapper';
 
-  const title = document.createElement('h1');
-  const copy = document.createElement('p')
-  copy.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ipsa quisquam similique itaque, esse quidem facere ad voluptas in! Unde aliquid veniam maxime nostrum a tenetur, quam ipsa magnam?'
-  title.textContent = 'Menu';
+  const { breakfast, lunch, dinner } = menuItems;
+
+  const breakfastMenu = breakfast.map(item => {
+    return `
+      <div class="menu-item">
+        <div class="menu-item__name">${item.name}</div>
+        <div class="menu-item__desc">${item.desc}</div>
+        <div class="menu-item__price">${item.price}</div>
+      </div>
+    `
+  });
+  menuItemList.innerHTML = breakfastMenu.join(' ');
 
   contentWrapper.appendChild(title);
-  contentWrapper.appendChild(copy);
   contentArea.appendChild(contentWrapper);
+  contentWrapper.appendChild(menuItemList);
 };
---
+++ src/js/components/menuitems/menuitems.js
@@ -0,0 +1,33 @@
+export const menuItems = {
+  breakfast: [
+    {
+      name: 'Two-Two-Two',
+      desc: 'Two eggs, two pancakes, and choice of meat',
+      price: 8
+    },
+    {
+      name: 'Pancakes - Short Stack',
+      desc: '3 pancakes, comes with maple syrup and a choice of meat',
+      price: 8
+    },
+    {
+      name: 'Pancakes - Full Stack',
+      desc: '5 pancakes, comes with maple syrup and a choice of meat',
+      price: 10
+    },
+  ],
+  lunch: [
+    {
+      name: 'Burger',
+      desc: 'Burger cooked medium well served with french fries',
+      price: 10
+    }
+  ],
+  dinner: [
+    {
+      name: 'Baked Salmon',
+      desc: 'Slow cooked salmon topped with a lemon sauce and served with mashed potatoes',
+      price: 15
+    }
+  ]
+};
